88af23a05fa83b3d98aacc9d5c07b5ccb54a6d1a
ratnajagadeesharava <ratnajagadeesh.arava@outlook.in>
2026-02-07 11:05:16 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,9 @@ fn main() {
         let mut command: String = Default::default();
         io::stdin().read_line(&mut command).unwrap();
         let cmd = command.trim().to_string();
+        if cmd == "exit"{
+            break ;
+        }
         println!("{cmd}: command not found");
     }
 }
