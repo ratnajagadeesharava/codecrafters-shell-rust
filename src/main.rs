6a97c530b463b82e87e168c4a53219c3f6bf79bd
ratnajagadeesharava <ratnajagadeesh.arava@outlook.in>
2026-02-07 12:23:57 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
+use std::collections::HashSet;
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
 fn main() {
+    let mut commands = HashSet::<String>::new();
+    commands.insert("exit".to_string());
+    commands.insert("type".to_string());
+    commands.insert("echo".to_string());
     loop {
+        
+
         print!("$ ");
         io::stdout().flush().unwrap();
         let mut commandLine: String = Default::default();
@@ -16,6 +23,14 @@ fn main() {
             "echo"=>{
                 println!("{}",&cmd[1..cmd.len()].join(" "))
             },
+            "type"=>{
+                if commands.contains(cmd[1]){
+                    println!("{} is a shell builtin",cmd[1]);
+                }
+                else{
+                    println!("{}: not found",cmd[1]);
+                }
+            }
             _=>{
                 println!("{}: command not found",cmd[0]);
             }
