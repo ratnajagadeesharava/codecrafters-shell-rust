bef3fac9085b178b0fce88fb808d71dc1e2cd62a
root <root@juniorj007.localdomain>
2026-02-12 12:06:56 +0000

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -18,14 +18,12 @@ fn main() {
     }
     // println!("{:?}",os_commands);
     loop {
-        
-
         print!("$ ");
         io::stdout().flush().unwrap();
         let mut commandLine: String = Default::default();
         io::stdin().read_line(&mut commandLine).unwrap();
         let cmdLine = commandLine.trim().to_string();
-        println!("{:?}",cmdLine);
+        // println!("{:?}",cmdLine);
         let cmd:Vec<&str>  = cmdLine.split(' ').collect();
         match cmd[0]{
             "exit"=> {
