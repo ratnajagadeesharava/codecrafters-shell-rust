0b82e351523a20d2afabea6f7545ad809f40a07c
root <root@juniorj007.localdomain>
2026-02-16 14:56:01 +0000

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,75 @@
 #![allow(warnings)]
 use std::{
-    collections::{HashMap, HashSet}, os::unix::fs::PermissionsExt, path::{self, Path}
+    collections::{HashMap, HashSet},
+    os::unix::fs::PermissionsExt,
+    path::{self, Path},
 };
 
 use std::io::{self, Write};
 use std::{env, fs};
-fn main() {
-    let mut commands = HashSet::<String>::new();
-    let mut os_commands: HashMap<String, String> = HashMap::<String, String>::new();
-    commands.insert("exit".to_string());
-    commands.insert("type".to_string());
-    commands.insert("echo".to_string());
+
+fn fill_os_commands(os_commands: &mut HashMap<String, String>) {
+    for entry in fs::read_dir("/usr/bin").unwrap() {
+        let path_str = entry.unwrap().path().to_string_lossy().to_string();
+        let entries: Vec<&str> = path_str.split("/usr/bin/").collect();
+        os_commands.insert(entries[1].to_string(), path_str);
+    }
+}
+fn fill_path_commands(os_commands: &mut HashMap<String, String>) {
     match env::var_os("PATH") {
         Some(val) => {
             let path = val.to_string_lossy().to_string();
             let items: Vec<&str> = path.split(':').collect();
-
             for item in items {
-                
                 if let Ok(val) = fs::metadata(item) {
                     if val.is_dir() {
                         for entry in fs::read_dir(item).expect("not  directory") {
                             let path_str = entry.unwrap().path().to_string_lossy().to_string();
-                            let meta_data = fs::metadata(&path_str).unwrap();
-
-                            if meta_data.is_file() {
-                                let p = Path::new(&path_str);
-                                
-                                let perm = meta_data.permissions();
-                                
+                            if let Ok(meta_data) = fs::metadata(&path_str) {
+                                if meta_data.is_file() {
+                                    let perm = meta_data.permissions();
+                                    if perm.mode() & 0o111 != 0 {
+                                        let fileName = Path::new(&path_str)
+                                            .file_name()
+                                            .unwrap()
+                                            .to_string_lossy()
+                                            .to_string();
+                                        os_commands.insert(fileName, path_str);
+                                    }
+                                }
                             }
                         }
                     }
                 }
-
-                // println!("{:?}",meta.is_dir());
             }
         }
         None => {
             println!("no path found");
         }
     }
-    for entry in fs::read_dir("/usr/bin").unwrap() {
-        let path_str = entry.unwrap().path().to_string_lossy().to_string();
-        let entries: Vec<&str> = path_str.split("/usr/bin/").collect();
-        os_commands.insert(entries[1].to_string(), path_str);
+}
+
+fn type_cmd(
+    cmd: &Vec<&str>,
+    commands: &HashSet<String>,
+    os_commands: &HashMap<String, String>,
+) -> String {
+    if commands.contains(cmd[1]) {
+        format!("{} is a shell builtin", cmd[1])
+    } else if os_commands.contains_key(cmd[1]) {
+        format!("{} is {}", cmd[1], os_commands.get(cmd[1]).unwrap())
+    } else {
+        format!("{}: not found", cmd[1])
     }
+}
+fn main() {
+    let mut commands = HashSet::<String>::new();
+    let mut os_commands: HashMap<String, String> = HashMap::<String, String>::new();
+    commands.insert("exit".to_string());
+    commands.insert("type".to_string());
+    commands.insert("echo".to_string());
+    fill_path_commands(&mut os_commands);
+
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
@@ -53,6 +77,7 @@ fn main() {
         io::stdin().read_line(&mut commandLine).unwrap();
         let cmdLine = commandLine.trim().to_string();
         let cmd: Vec<&str> = cmdLine.split(' ').collect();
+        let argsLength = cmd.len();
         match cmd[0] {
             "exit" => {
                 break;
@@ -61,16 +86,18 @@ fn main() {
                 println!("{}", &cmd[1..cmd.len()].join(" "))
             }
             "type" => {
-                if commands.contains(cmd[1]) {
-                    println!("{} is a shell builtin", cmd[1]);
-                } else if os_commands.contains_key(cmd[1]) {
-                    println!("{} is {}", cmd[1], os_commands.get(cmd[1]).unwrap());
-                } else {
-                    println!("{}: not found", cmd[1]);
-                }
+                let val = type_cmd(&cmd, &commands, &os_commands);
+                println!("{val}");
             }
-            _ => {
-                println!("{}: command not found", cmd[0]);
+            val  => {
+                if os_commands.contains_key(val){
+                    let err = exec::Command::new(os_commands.get(val).unwrap()).args(&cmd[1..argsLength]).exec();
+                    println!("error:{:?}",err);
+
+                }
+                else{
+                    println!("{}: command not found", cmd[0]);
+                }
             }
         }
     }
